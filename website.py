12ba7893cae26ea5e981de592213da019116ee6c
DrSuperMario <mario@pythonslack.com>
2020-03-31 17:05:16 +0300

    First commit

---
+++ website.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+#impordi flaski classi objekti flaski raamatust ja html renderdaja
+
+app=Flask(__name__)
+#kirjutan välja flaski objekti nime
+
+@app.route('/')
+#millisele aadressile soovid runnida nt. /about/ 
+#kui soovid veel lehti siis lisa veel route objekte
+# kuid tuleb määrata ka funktsioon
+def home():
+    return render_template("home.html")
+#funktsioon mis väljastab info lehele
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+#teine leht
+
+if __name__ =="__main__":
+    app.run(debug=True)
+#kontrollin kas nimi on võrdne mainiga ja käivitan skripti
