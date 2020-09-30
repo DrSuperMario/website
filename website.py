7b3462db99f4bd0121234c7b3bffada496c0554a
DrSuperMario <mario@pythonslack.com>
2020-09-30 12:06:34 +0000

    updated wsgi.py and pic

--- website.py
+++ website.py
@@ -31,5 +31,6 @@ def fcssignals():
 '''
 
 if __name__ =="__main__":
-    app.run(debug=True, port=5050)
+    app.run(debug=True, host='0.0.0.0', port=5050)
 #kontrollin kas nimi on võrdne mainiga ja käivitan skripti
+			
