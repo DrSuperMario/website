30f9492dea0b82a8fc1390968a8e44737576cd10
mario muuk <62722269+DrSuperMario@users.noreply.github.com>
2021-01-14 19:39:45 +0200

    Updated portfolio

--- website.py
+++ website.py
@@ -31,6 +31,6 @@ def fcssignals():
 '''
 
 if __name__ =="__main__":
-    app.run(debug=True, host='0.0.0.0', port=5050)
+    app.run(debug=False, host='0.0.0.0', port=5050)
 #kontrollin kas nimi on võrdne mainiga ja käivitan skripti
 			
